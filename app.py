b8b8ed15e1267ff289909581bb1b7f60f6ed7924
ShivamY-30 <113720988+ShivamY-30@users.noreply.github.com>
2024-02-19 17:19:13 +0530

    Twelth Change

--- app.py
+++ app.py
@@ -149,3 +149,38 @@ if uploaded_files is not None:
         #     st.pyplot(fig)
 
 
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: black;
+color: white;
+text-align: center;
+}
+p{
+text-align:center;
+}
+</style>
+<div class="footer">
+<p>Developed by :  <a style=' text-align: center;' href="http://myiportfolio.000webhostapp.com/index.php" target="_blank">Shivam Yadav</a></p>
+<p>Contact for details and discussion :  <a style='text-align: center;' target="_blank">yadavshivamp90671@gmail.com</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
+
+
+
